5c84f615cbb3cceaaeb8c5a158ece7beb94ae718
wanghaitao <15124505701@163.com>
2018-11-24 16:35:42 +0800

    测试JsonMapper及获取Spring上下文工具

--- src/main/java/com/mmall/controller/TestController.java
+++ src/main/java/com/mmall/controller/TestController.java
@@ -1,16 +1,18 @@
 package com.mmall.controller;
 
+import com.mmall.common.ApplicationContextHelper;
 import com.mmall.common.JsonData;
+import com.mmall.dao.SysAclModuleMapper;
 import com.mmall.exception.ParamException;
 import com.mmall.exception.PermissionException;
+import com.mmall.model.SysAclModule;
 import com.mmall.param.TestVo;
 import com.mmall.util.BeanValidator;
+import com.mmall.util.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.MapUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/test")
@@ -28,16 +30,9 @@ public class TestController {
     @ResponseBody
     public JsonData validate(TestVo vo) throws ParamException {
         log.info("validate");
-//        try {
-//            Map<String, String> map = BeanValidator.validateObject(vo);
-//            if (MapUtils.isEmpty(map)) {
-//                for (Map.Entry<String, String> entry: map.entrySet()){
-//                    log.info("{}->{}",entry.getKey(), entry.getValue());
-//                }
-//            }
-//        } catch (Exception e) {
-//
-//        }
+        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
+        log.info(JsonMapper.obj2String(module));
         BeanValidator.check(vo);
         return JsonData.success("test validate");
     }
