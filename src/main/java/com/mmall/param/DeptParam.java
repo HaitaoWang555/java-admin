5c47c9e232527177e67b572b8d3d27ade98f2295
wanghaitao <15124505701@163.com>
2018-11-26 23:21:07 +0800

    新增部门接口开发

---
+++ src/main/java/com/mmall/param/DeptParam.java
@@ -0,0 +1,29 @@
+package com.mmall.param;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@ToString
+public class DeptParam {
+
+    private Integer id;
+
+    @NotBlank(message = "部门名称不能为空")
+    @Length(max = 15, min = 2, message = "部门名称长度在2-15个字")
+    private String name;
+
+    private Integer parentId = 0;
+
+    @NotNull(message = "展示顺序不可为空")
+    private Integer seq;
+
+    @Length(max = 150, message = "备注长度要在150字之内")
+    private String remark;
+}
