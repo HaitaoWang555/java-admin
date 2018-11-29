116fa70e1056d1c943ce5439e0d4be4c7c3eca48
wanghaitao <15124505701@163.com>
2018-11-30 04:36:10 +0800

    新增用户接口

---
+++ src/main/java/com/mmall/service/SysUserService.java
@@ -0,0 +1,73 @@
+package com.mmall.service;
+
+import com.google.common.base.Preconditions;
+import com.mmall.dao.SysUserMapper;
+import com.mmall.exception.ParamException;
+import com.mmall.model.SysUser;
+import com.mmall.param.UserParam;
+import com.mmall.util.BeanValidator;
+import com.mmall.util.MD5Util;
+import com.mmall.util.PasswordUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Service
+public class SysUserService {
+
+    @Resource
+    private SysUserMapper sysUserMapper;
+    public void save(UserParam param) {
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())) {
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())) {
+            throw new ParamException("邮箱已被占用");
+        }
+        String password = PasswordUtil.randomPassword();
+        //TODO:
+        password = "12345678";
+        String encryptedPassword = MD5Util.encrypt(password);
+        SysUser user = SysUser.builder().username(param.getUsername())
+                .telephone(param.getTelephone()).mail(param.getMail())
+                .password(encryptedPassword).deptId(param.getDeptId())
+                .status(param.getStatus()).remark(param.getRemark()).build();
+        user.setOperator("system"); //TODO:
+        user.setOperateIp("127.0.0.1"); //TODO:
+        user.setOperateTime(new Date());
+
+        // TODO: sendEmail
+
+        sysUserMapper.insertSelective(user);
+    }
+
+    public void update(UserParam param) {
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())) {
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())) {
+            throw new ParamException("邮箱已被占用");
+        }
+        SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
+        SysUser after = SysUser.builder().id(param.getId())
+                .username(param.getUsername()).telephone(param.getTelephone())
+                .mail(param.getMail()).deptId(param.getDeptId())
+                .status(param.getStatus()).remark(param.getRemark()).build();
+        after.setOperator("update-system");
+        after.setOperateIp("127.0.0.1");
+        after.setOperateTime(new Date());
+        sysUserMapper.updateByPrimaryKeySelective(after);
+    }
+
+    private boolean checkEmailExist(String mail, Integer id) {
+        return false;
+    }
+
+    private boolean checkTelephoneExist(String telephone, Integer id) {
+        return false;
+    }
+}
